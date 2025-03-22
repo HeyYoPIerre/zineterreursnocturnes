497ef5d1ca604455626ed47eb1a1324829ae075b
Damien FOSSEY <fossey.d@outlook.com>
2025-03-22 20:55:05 +0100

    same author as title in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="zineterreursnocturne">
+    <meta name="author" content="ZineTerreursNocturnes">
     <meta http-equiv="Cache-control" content="public">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>ZineTerreursNocturnes</title>
