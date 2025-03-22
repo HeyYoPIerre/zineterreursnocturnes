05d38db823622f04c50a2568dcae1315bbf0aa8f
Damien FOSSEY <fossey.d@outlook.com>
2025-03-22 20:01:10 +0100

    fix app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-
-  <head>
+<head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Zine terreurs nocturnes</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="{{ mix('/resources/css/app.css') }}" rel="stylesheet">
+    <meta name="author" content="zineterreursnocturne">
+    <meta http-equiv="Cache-control" content="public">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>ZineTerreursNocturnes</title>
+
+    @vite(['resources/sass/app.scss'])
     @stack('style')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-  </head>
+</head>
 
-  <body class="d-flex flex-column min-vh-100 @yield('body_class')">    
-    
-    @include('partials.header')
-    {{--Content--}}
-    <div class="d-flex flex-column flex-grow-1">
-      @yield('content')
-    </div>
-    {{--Content--}} 
-    @include('partials.footer')
-    @vite('resources/js/app.js')
-    @stack('scripts')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+    <body class="d-flex flex-column min-vh-100 @yield('body_class')">
+        @include('partials.header')
 
+        <div class="d-flex flex-column flex-grow-1">
+            @yield('content')
+        </div>
 
+        @include('partials.footer')
+
+        @vite(['resources/js/app.js'])
+        @stack('scripts')
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    </body>
 </html>
