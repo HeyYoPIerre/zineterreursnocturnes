b3a15a7fa559df47027d772d961d03c87fafa9da
The Boss <pierre.mitou1@gmail.com>
2025-03-23 19:22:44 +0100

    install tinymce

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
@@ -12,5 +13,13 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'node_modules/tinymce/',
+                    dest: '../node_modules/'
+                }
+            ]
+        })
     ],
 });
